2bfc287fc15f4a3f0ddd2a20894ff8dc6398828b
Charles <creidbrowniii@gmail.com>
2025-08-11 17:43:05 -1000

    integrated user input for function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 
-function generateRandomPassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+const lengthInput = document.getElementById("lengthInput");
+const includeLowercase = document.getElementById("includeLowercase");
+const includeUppercase = document.getElementById("includeUppercase");
+const includeNumbers = document.getElementById("includeNumbers");
+const includeSymbols = document.getElementById("includeSymbols");
+const generateButton = document.getElementById("generateButton");
+const result = document.getElementById("result");
+
+
+generateButton.onclick = function() {
     let password = "";
     let allowedChars = "";
 
@@ -8,34 +17,25 @@ function generateRandomPassword(length, includeLowercase, includeUppercase, incl
     numberChars = "0123456789";
     symbolChars = "~-_+=,.<>()?/[]{}";
 
-    allowedChars += includeLowercase ? lowercaseChars : "";
-    allowedChars += includeUppercase ? uppercaseChars : "";
-    allowedChars += includeNumbers ? numberChars : "";
-    allowedChars += includeSymbols ? symbolChars : "";
+    allowedChars += includeLowercase.checked ? lowercaseChars : "";
+    allowedChars += includeUppercase.checked ? uppercaseChars : "";
+    allowedChars += includeNumbers.checked  ? numberChars : "";
+    allowedChars += includeSymbols.checked ? symbolChars : "";
 
-    if (length <= 0) {
-        console.log("Please enter a length longer than 0.");
-        return "";
+    let passLength = lengthInput.value;
+    if (passLength <= 0) {
+        result.textContent = "Please enter a length longer than 0.";
     }
-    if (allowedChars.length === 0) {
-        console.log("Please select at least ONE set of characters.");
-        return "";
+    else if (allowedChars.length === 0) {
+        result.textContent = "Please select at least ONE set of characters.";
     }
-
-    for (let i = 0; i < length; i++) {
-        randomIndex = Math.floor(Math.random() * allowedChars.length);
-        password += allowedChars[randomIndex];
+    else {
+        console.log(allowedChars);
+        for (let i = 0; i < passLength; i++) {
+            randomIndex = Math.floor(Math.random() * allowedChars.length);
+            password += allowedChars[randomIndex];
+        }
+        result.textContent = password;
     }
-
-    return password;
+    
 }
-
-const length = 10;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
-
-const password = generateRandomPassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
-
-console.log(`Generated password: ${password}`);
